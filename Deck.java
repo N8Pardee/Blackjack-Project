9d6590b11ae9570535e2e16aeeb759769cd07a33
N8Pardee <nate.pardee@gmail.com>
2025-01-27 19:46:08 -0600

    forgot to add all

--- Deck.java
+++ Deck.java
@@ -1,7 +1,16 @@
 package blackJackGame;
 
+import java.util.ArrayList;
+
 //parent of card class and contains operations for shuffling 
 
 public class Deck {
+	
+	private ArrayList<Card> deck;
+	
+	public Deck() {
+		
+		deck = new ArrayList<Card>();
+	}
 
 }
