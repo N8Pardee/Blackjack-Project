de95f0baf90b212fe89eb29bf7d4007e750482d9
N8Pardee <nate.pardee@gmail.com>
2025-02-28 20:27:51 -0600

    created constructor for dealer that utilizes Person class methods

--- Dealer.java
+++ Dealer.java
@@ -2,6 +2,17 @@ package blackJackGame;
 
 //child of person. handles dealer operations: Hit, Stand, Stay
 
-public class Dealer {
+public class Dealer extends Person{
 
+	//create new hand for dealer
+	public Hand dealerHand = new Hand();
+	
+	Dealer(){
+		//name dealer with the super keyword. This works by calling parent classes name setting fn
+		super.setName("Dealer");
+	
+	}
+	
+	
+	
 }
