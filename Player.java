ca9297ceb7f4933465f06294e61d73af87e5b14c
N8Pardee <nate.pardee@gmail.com>
2025-02-28 20:55:12 -0600

    Stopping 2.28 working on Person/game logic

--- Player.java
+++ Player.java
@@ -1,7 +1,21 @@
 package blackJackGame;
 
 //child of person class and handles player operations
-public class Player {
+public class Player extends Person{
+	
+	//constructor
+	public Player(){
 		
+		super.setName("Player");
+	}
+	
+	//create decision based on choices using integer from user input. this method will return a boolean value dependent on 
+	//the integer used
+
+	public boolean gameDecision(int choice) {
+		
+		return true;
+		
+	}
 
 }
